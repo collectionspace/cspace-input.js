d0b002c53fcb730c63d6b79b55967d983d1044f9
Ray Lee <rhlee@berkeley.edu>
2016-09-20 21:31:19 -0700

    DRYD-48: Add type prop to Button.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import styles from '../../styles/cspace-input/Button.css';
 
 /**
  * A button.
  */
-export default props => (
-  <button
-    className={styles.common}
-    type="button"
-    {...props}
-  />
-);
+export default function Button(props) {
+  const {
+    type,
+    ...remainingProps,
+  } = props;
+
+  return (
+    <button
+      className={styles.common}
+      type={type}
+      {...remainingProps}
+    />
+  );
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+};
+
+Button.defaultProps = {
+  type: 'button',
+};
