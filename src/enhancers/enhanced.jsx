820c922560f5f53f6ee34ace4949016046c1fc1e
Ray Lee <rhlee@berkeley.edu>
2016-09-21 01:32:50 -0700

    DRYD-48: Remove withValueAtPath enhancer.

--- src/enhancers/enhanced.jsx
+++ src/enhancers/enhanced.jsx
@@ -3,9 +3,8 @@ import changeable from '../enhancers/changeable';
 import committable from '../enhancers/committable';
 import labelable from '../enhancers/labelable';
 import repeatable from '../enhancers/repeatable';
-import withValueAtPath from '../enhancers/withValueAtPath';
 
 /**
  * Applies a common set of enhancers used by all cspace-input components.
  */
-export default compose(repeatable, labelable, withValueAtPath, committable, changeable);
+export default compose(repeatable, labelable, committable, changeable);
