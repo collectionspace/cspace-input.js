998cd7c77f8c7c5616b659d340f536f2b8c0ae9e
Ray Lee <rhlee@berkeley.edu>
2016-10-03 21:00:40 -0700

    DRYD-51: Stub out remaining input components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import LineInput from './components/LineInput';
+
 export { default as Button } from './components/Button';
 export { default as CustomCompoundInput } from './components/CustomCompoundInput';
 export { default as CompoundInput } from './components/CompoundInput';
 export { default as Label } from './components/Label';
-export { default as LineInput } from './components/LineInput';
+// export { default as LineInput } from './components/LineInput';
 export { default as MultilineInput } from './components/MultilineInput';
 export { default as PasswordInput } from './components/PasswordInput';
 export { default as ReadOnlyInput } from './components/ReadOnlyInput';
@@ -10,3 +12,14 @@ export { default as RepeatingInput } from './components/RepeatingInput';
 export { default as TabularCompoundInput } from './components/TabularCompoundInput';
 export { default as TextInput } from './components/TextInput';
 export { default as getPath } from './helpers/getPath';
+
+export { LineInput };
+
+// Stubs
+
+export const IDGeneratorInput = LineInput;
+export const DateInput = LineInput;
+export const StructuredDateInput = LineInput;
+export const StaticControlledInput = LineInput;
+export const VocabularyControlledInput = LineInput;
+export const AuthorityControlledInput = LineInput;
