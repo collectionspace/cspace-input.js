695ff709d5e51b099121a3d8587cced1b834fd03
Ray Lee <rhlee@berkeley.edu>
2017-01-10 20:55:13 -0800

    NOJIRA: Fix MiniButton not accepting className prop.

--- src/components/MiniButton.jsx
+++ src/components/MiniButton.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 import styles from '../../styles/cspace-input/MiniButton.css';
+
+const propTypes = {
+  className: PropTypes.string,
+};
 
 /**
  * A small button attached to an input.
  */
 export default function MiniButton(props) {
+  const {
+    className,
+  } = props;
+
+  const classes = classNames(styles.common, className);
+
   return (
     <button
       {...props}
-      className={styles.common}
+      className={classes}
       type="button"
     />
   );
 }
+
+MiniButton.propTypes = propTypes;
