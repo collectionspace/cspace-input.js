eea74b3091e91ef50047db32b74037cd05f8bac9
Ray Lee <rhlee@berkeley.edu>
2018-10-30 22:47:41 -0700

    NOJIRA: Change autocomplete back to 'nope'.

--- src/components/LineInput.jsx
+++ src/components/LineInput.jsx
@@ -101,7 +101,10 @@ export default class LineInput extends Component {
     const input = (
       <input
         // In Chrome, having autocomplete="off" on the parent form doesn't seem to be enough.
-        autoComplete="off"
+        // Setting autocomplete="off" on the input also doesn't work. Use the autocomplete="nope"
+        // hack, as suggested here:
+        // https://developer.mozilla.org/en-US/docs/Web/Security/Securing_your_site/Turning_off_form_autocompletion
+        autoComplete="nope"
         {...remainingProps}
         className={className}
         disabled={readOnly}
