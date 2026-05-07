e20c2d9b10876464c114fdd4ae6d148b11a34635
spiros dimopulos <spiros@wantedpixel.com>
2026-05-07 15:27:02 +0300

    DRYD-1923: render label as legend in InputTable; wrap it in a fieldset;

--- src/components/InputTable.jsx
+++ src/components/InputTable.jsx
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import InputTableHeader from './InputTableHeader';
 import InputTableRow from './InputTableRow';
 import styles from '../../styles/cspace-input/InputTable.css';
+import Label from './Label';
 
 const propTypes = {
   children: PropTypes.node,
   embedded: PropTypes.bool,
+  id: PropTypes.string,
+  label: PropTypes.node,
   renderLabel: PropTypes.func,
   renderAriaLabel: PropTypes.func,
 };
@@ -14,6 +17,8 @@ const propTypes = {
 const defaultProps = {
   children: undefined,
   embedded: undefined,
+  id: undefined,
+  label: undefined,
   renderLabel: undefined,
   renderAriaLabel: undefined,
 };
@@ -22,12 +27,29 @@ export default function InputTable(props) {
   const {
     children,
     embedded,
+    id,
+    label,
     renderLabel,
     renderAriaLabel,
   } = props;
 
+  let legend = null;
+
+  if (React.isValidElement(label) && label.type === Label) {
+    // Extract the label's children, className, id
+    // and render it as legend instead of an orphaned label
+    legend = (
+      <legend className={label.props.className} id={label.props.id}>
+        {label.props.children}
+      </legend>
+    );
+  } else if (label) {
+    legend = <legend>{label}</legend>;
+  }
+
   return (
-    <div className={styles.common}>
+    <fieldset className={styles.common} id={id}>
+      {legend}
       <InputTableHeader embedded={embedded} renderLabel={renderLabel}>
         {children}
       </InputTableHeader>
@@ -35,9 +57,10 @@ export default function InputTable(props) {
       <InputTableRow embedded={embedded} renderAriaLabel={renderAriaLabel}>
         {children}
       </InputTableRow>
-    </div>
+    </fieldset>
   );
 }
 
 InputTable.propTypes = propTypes;
 InputTable.defaultProps = defaultProps;
+InputTable.useLegend = true;
